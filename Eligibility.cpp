fdd9d9ac84c2fb0b28f9ca5d98a739b67aba0b87
Thomas Townsley <thomasdtownsley@gmail.com>
2019-01-09 17:20:16 -0600

    commits and changes

---
+++ Eligibility.cpp
@@ -0,0 +1,43 @@
+#include <iostream>
+using namespace std; 
+
+int main()
+ {
+ int x;
+
+ cout << "Enter a number: ";
+ cin >> x;
+ if( x > 100 )
+ cout << "That number is greater than 100\n";
+ return 0;
+ }
+
+/*   
+    //variable declarations
+    int i; 
+    int k, j; 
+
+    cin >> i;
+    if (i >= 2010){
+      cout << "eligible\n";
+    }
+    
+    cin >> j;
+    else if (j >= 1991){
+      cout << "ineligible\n"; 
+    }
+
+    else if (k >= 5){
+      cout << "ineligible\n"; 
+    }
+
+    cin >> k;
+      else (k < 5){
+      cout << "coach petitions\n"; 
+    }
+
+    return 0;  
+} 
+*/
+
+
