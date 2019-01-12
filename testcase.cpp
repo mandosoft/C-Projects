2f37dade57da24c293c67deb33e49a00e2b6baee
Thomas Townsley <thomasdtownsley@gmail.com>
2019-01-12 08:42:02 -0600

    commits

--- testcase.cpp
+++ testcase.cpp
@@ -4,36 +4,32 @@
 using namespace std;
 
 /*
-
 Input test data 
-
 3
 EligibleContestant 2013/09/01 1995/03/12 10
 IneligibleContestant 2009/09/01 1990/12/12 50
 PetitionContestant 2009/09/01 1990/12/12 35
-
 */ 
-
 
 int main(){
 
     int i, k, j, m; 
     std::string s; 
     
-    cin >> i >> s >> k >> j >> m;
+   while ( std::cin >> i >> s >> k >> j >> m){
 
-    cout << s;  
+    cout << i << s << k << j << m; 
 
     if (k >= 2010/01/01){
-      cout << "eligible\n";
+      cout << s << "eligible\n";
     }
     else if (j > 1991/01/01 || m >= 5 ){
-      cout << "ineligible\n"; 
+      cout << s << "ineligible\n"; 
     }
     else {
-      cout << "coach petitions\n";
+      cout << s << "coach petitions\n";
     }
-
+   }
   return 0;  
 
 
