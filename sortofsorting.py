9e2d72f9bb2da8737c1b79b50d5836be924914ed
Thomas Townsley <thomasdtownsley@gmail.com>
2019-01-28 13:45:24 -0600

    additions

--- sortofsorting.py
+++ sortofsorting.py
@@ -1,11 +1,16 @@
 #Write a program that sorts a list of student last names, 
 #but the sort only uses the first two letters of the name. 
 #Nothing else in the name is used for sorting.
-
-infile=input().split()
-while infile != 0: 
-    for i in infile:
+import sys
+while True:
+    try:
+        infile=sys.stdin.readlines() #read infile as ints
+        if infile==0: # EOF sentinel  
+            break
+        values = [input() for i in infile] #creates array by taking input and and iterating over it 
+        sorted(values, key=lambda x:x[0:2]) #sort input based on first two indexes in array 
+        print(values)
+        print('\n'.join(values)) 
         print()
-
-
-
+    except:EOFError
+    break
